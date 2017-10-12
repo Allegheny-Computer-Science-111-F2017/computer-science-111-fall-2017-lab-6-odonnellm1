463ac71c8abba467e65b2f0a5a937ed7619c3ad4
Molly O'Donnell <odonnellm@aldenv162.allegheny.edu>
2017-10-12 15:53:51 -0400

    initial labsix commit

--- src/main/java/labsix/ManipulateDna.java
+++ src/main/java/labsix/ManipulateDna.java
@@ -3,13 +3,31 @@ package labsix;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
+
+/**
+ * This class produces a simple DNA Manipulation.
+ * The program will ask the user to enter a DNA-sequence
+ * And then the program will manipulate the sequence
+ * Multiple different ways/
+ *
+ * <p>Bugs: This program has no known bugs.
+ *
+ * @author Molly O'Donnell
+ */
 
 public class ManipulateDna {
 
+  /**
+   * This program will produce a manipulated DNA sequence.
+   *
+   * @author Molly O'Donnell
+   */
+
   public static void main(String[] args) {
     // display the name of the programmer and the date
-    System.out.println("Gregory M. Kapfhammer " + new Date());
+    System.out.println("Molly O'Donnell " + new Date());
     // declare the starting file and scanner
     File dnaInputsFile = null;
     Scanner scanner = null;
@@ -21,16 +39,46 @@ public class ManipulateDna {
       System.out.println("Unable to locate file");
     }
     // Step One: Read in and display the chosen DNA string
-
+    String dna = scanner.nextLine();
+    System.out.println("Okay, I am going to manipulate the DNA string " + dna);
+    System.out.println(); 
+    System.out.println("Enter a string containing only C, G, T, and A: " + dna);
+   
     // Step Two: Compute the complement of the DNA String
+    String comp1 = dna.replace('a', 'T');
+    String comp2 = comp1.replace('t', 'A');
+    String comp3 = comp2.replace('c', 'G');
+    String comp4 = comp3.replace('g', 'C');
+    comp4 = comp4.toUpperCase();
+    System.out.println("The complement of " + dna + " is " + comp4);
 
     // Step Three: Insert a randomly chosen DNA letter into the DNA String
-
+    Random random = new Random();
+    int num1 = random.nextInt(4);
+    char randomLetter = dna.toUpperCase().charAt(num1);
+    int n6 = random.nextInt(4);
+    String inDna = dna.substring(0, n6) + randomLetter + dna.substring(n6);
+    inDna = inDna.toUpperCase();
+    System.out.println("Inserting " + randomLetter + " at position " + n6 + " gives " + inDna);    
+ 
     // Step Four: Delete a DNA letter from a randomly chosen position in the DNA string
-
+    int num2 = random.nextInt(4);
+    String deletedDna = dna.substring(0, num2) + dna.substring(num2 + 1);
+    deletedDna = deletedDna.toUpperCase();
+    System.out.println("Deleting from position " + num2 + " gives " + deletedDna);
     // Step Five: Change a random position in the DNA String to a randomly chosen DNA letter
+    int num3 = random.nextInt(4);
+    int num4 = random.nextInt(4);
+    char randomPosition = dna.charAt(num3);
+    char randomLetter2 = dna.charAt(num4);
+    String changedPosition = dna.replace(randomPosition, randomLetter2);
+    changedPosition = changedPosition.toUpperCase();
+    System.out.println("Changing position " + num3 + " gives " + changedPosition);
+    System.out.println();
 
     // Step Six: Display a final thankyou message
+    System.out.println("Thanks for using the Manipulation program.");
+    System.out.println("Have an amazing day.");
 
   }
 
--- writing/reflection.md
+++ writing/reflection.md
@@ -1,4 +1,20 @@
-# Reflection by
+# Reflection by Molly O'Donnell
 
-This lab required me to write a file called `ManipulateDNA`. Please review the
-Laboratory Assignment Sheet and add the required details to this paragraph.
+## October 12, 2017
+
+This lab required me to write a file called `ManipulateDNA`. This program would
+ask the user to input a DNA sequence and then the program would manipulate the
+sequence in different ways. This lab allowed me to practice and master the
+commands that we have been working on throughout the semester. Using the
+command new Scanner() allowed the information that was being input by the user,
+to be pulled from the external file and be used in the program. I also
+practiced assgning varibales like int and char and having many mutations to the
+original DNA code.
+
+My biggest challenge was in step five when I used the replace command to change
+a random position in the DNA String to a randomly chosen DNA letter. The
+problem with this step is that in order to use the replace command, you need
+your variables in char form. Unfortunately, the variables that I imported were
+in int form. My solution to this problem, with the help of the TAs, was to
+convert my int varibales to char varibales through using the command, char
+newname = dna.charAt(int).
